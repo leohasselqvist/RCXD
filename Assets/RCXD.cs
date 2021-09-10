5bfae07226ceda2d4759fb9271fa0874cd42b271
Leo <leo.hasselqvist@elev.ntig.se>
2021-09-10 18:46:12 +0200

    Imported the started project

---
+++ Assets/RCXD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCXD : MonoBehaviour
+{
+
+    public float baseSpeed = 1;
+    public float speedMod = 1;
+    public float acceleration = 1;
+
+    public Rigidbody2D rb;
+
+    private float currentSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSpeed < baseSpeed) {
+            currentSpeed += acceleration / 100;
+        }
+        else { currentSpeed = baseSpeed; }
+        var movement_v = currentSpeed * speedMod;
+        rb.velocity = new Vector2(0, movement_v);
+        
+
+    }
+}
